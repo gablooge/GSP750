e0da38f0e840457f116bfca53e47fc6c33c10ff1
Samsul Hadi <samsulhadikediri@gmail.com>
2023-03-31 13:08:58 +0700

    fix: 3

--- 3.sh
+++ 3.sh
@@ -13,4 +13,6 @@ PROJECT_ID=$(gcloud config list project --format "value(core.project)")
 
 sed -i "s/PROJECT_ID/$PROJECT_ID/g" main.tf
 
-terraform apply -destroy -auto-approve
+terraform apply -auto-approve
+
+terraform destroy -auto-approve
